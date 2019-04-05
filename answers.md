72fef8b1ab520af6821fb08fbff1e5fcc1a446ea
Naman Hemal Kapasi <kapasinh@s.dcsdk12.org>
2019-04-05 07:55:14 -0600

    activity3

---
+++ answers.md
@@ -0,0 +1,23 @@
+1. A deck is made up of cards
+2. 6
+3. ranks from a to king, 4 suits, and 10 point values with 10 repeated for 10-ace
+4. yes because thats how the deck is created
+
+
+
+1.  public static boolean flip(){
+        n = (int)math.random()*3;
+        if(n = 2 || n = 1){
+            return true;
+        }else {
+            return false;
+        }
+
+2. public permutations(int []. int []){
+    for(int i = 0; i < [].length; i+=){
+    }
+}
+
+
+
+3. 0,1,2
--- src/Shuffler.java
+++ src/Shuffler.java
@@ -1,6 +1,8 @@
 /**
  * This class provides a convenient way to test shuffling methods.
  */
+import java.util.Random;
+
 public class Shuffler {
 
 	/**
@@ -17,7 +19,7 @@ public class Shuffler {
 	public static void main(String[] args) {
 		System.out.println("Results of " + SHUFFLE_COUNT +
 								 " consecutive perfect shuffles:");
-		int[] values1 = {0, 1, 2, 3};
+		int[] values1 = {0, 1, 2, 3,4,5,6,7,8,9};
 		for (int j = 1; j <= SHUFFLE_COUNT; j++) {
 			perfectShuffle(values1);
 			System.out.print("  " + j + ":");
@@ -51,6 +53,22 @@ public class Shuffler {
 	 */
 	public static void perfectShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int k = 0;
+		for(int j = 0; j<shuffled.length/2; j++){
+			shuffled[k] = values[j];
+			k = k+2;
+		}
+		k = 1;
+		for(int j = shuffled.length/2; j<shuffled.length; j++){
+			shuffled[k] = values[j];
+			k = k+2;
+		}
+
+
+		for(int z = 0; z<shuffled.length; z++){
+			values[z] = shuffled[z];
+		}
 	}
 
 	/**
@@ -66,5 +84,13 @@ public class Shuffler {
 	 */
 	public static void selectionShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for(int k = values.length-1; k>0; k--){
+			int r = (int)(Math.random()*(k+1));
+			int n = values[k];
+			values[k] = values[r];
+			values[r] = n;
+
+		}
+
 	}
 }
